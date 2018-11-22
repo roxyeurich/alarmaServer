5a293ab6a2cf6c357519134a7805bec0625bdd26
roxyeurich <32582901+roxyeurich@users.noreply.github.com>
2018-11-21 16:21:04 -0800

    Add files via upload

---
+++ getReward.php
@@ -0,0 +1,27 @@
+<?php
+//header('Access-Control-Allow-Origin: *'); 
+try {
+  $conn = new PDO("mysql:host=d6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=qefvl1o77u29l7gz","zuaks09ibij50mi6","ow722kdbo87l4aok");
+} catch (PDOException $e){
+  echo "Error".$e->getMessage();
+}
+
+$group_id =$_POST["group_id"];
+
+$query = "SELECT * FROM rewards WHERE group_id='$group_id' ORDER BY id DESC LIMIT 0, 5";
+
+//"SELECT * FROM users WHERE username='$username' AND password = '$password';
+
+//USE THIS TO TEST IF INFO IS GOING INTO DATABASE:
+//$query = "INSERT INTO users (email, password, username, status) VALUES ('test', 'test', 'test', 1)";
+
+$result = $conn->query($query);
+if($result){
+  $rewar=$result->fetchAll();
+  echo json_encode($rewar); 
+  
+} else {
+  echo json_encode(false);
+}
+
+?>
