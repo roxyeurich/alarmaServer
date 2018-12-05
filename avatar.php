89628c536ca584d7e1b41e8a80ed5cecc76002c8
roxyeurich <32582901+roxyeurich@users.noreply.github.com>
2018-12-04 18:54:06 -0800

    Add files via upload

---
+++ avatar.php
@@ -0,0 +1,24 @@
+<?php
+header('Access-Control-Allow-Origin: *'); 
+try {
+  $conn = new PDO("mysql:host=d6q8diwwdmy5c9k9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=qefvl1o77u29l7gz","zuaks09ibij50mi6","ow722kdbo87l4aok");
+} catch (PDOException $e){
+  echo "Error".$e->getMessage();
+}
+
+$userid =$_POST["user_id"];
+$avatar =$_POST["avatar"];
+
+$query = "UPDATE users SET user_id='$userid' WHERE avatar='$avatar'";
+echo $query;
+  
+$result = $conn->query($query);
+if($result){
+  echo json_encode(true); 
+  
+  
+} else {
+  echo json_encode(false);
+}
+
+?>
