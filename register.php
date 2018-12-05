90979ddbaada342fb90dbab57addd5e343b9f8a6
roxyeurich <32582901+roxyeurich@users.noreply.github.com>
2018-12-05 11:01:22 -0800

    Add files via upload

--- register.php
+++ register.php
@@ -12,22 +12,44 @@ $username =$_POST["username"];
 //$location_main =$_POST["location_main"];
 
 
-$query = "INSERT INTO users (email, password, username, admin) VALUES ('$email', '$password', '$username', 1)";
-
+$query = "SELECT * FROM users WHERE email='$email'";
 
 //"SELECT * FROM users WHERE username='$username' AND password = '$password';
+
+//USE THIS TO TEST IF INFO IS GOING INTO DATABASE:
 //$query = "INSERT INTO users (email, password, username, status) VALUES ('test', 'test', 'test', 1)";
 
 $result = $conn->query($query);
 if($result){
-  $userid=$conn->lastInsertId();
-  echo json_encode(array(
-    'status'=>true,
-    'id'=>$userid,
-  ));
+  $users=$result->fetchAll();
+  
+  if (!empty($users)){
+    echo json_encode(false);  
+  } else {
+    
+    $query = "INSERT INTO users (email, password, username, admin) VALUES ('$email', '$password', '$username', 1)";
+
+
+    //"SELECT * FROM users WHERE username='$username' AND password = '$password';
+    //$query = "INSERT INTO users (email, password, username, status) VALUES ('test', 'test', 'test', 1)";
+
+    $result = $conn->query($query);
+    if($result){
+      $userid=$conn->lastInsertId();
+      echo json_encode(array(
+        'status'=>true,
+        'id'=>$userid,
+      ));
+
+    } else {
+      echo json_encode(false);
+    }
+  }
+  
   
 } else {
   echo json_encode(false);
 }
+
 
 ?>
