578f1e58a6e062c0679d8af4a57fd7e20bcf7070
roxyeurich <32582901+roxyeurich@users.noreply.github.com>
2018-12-07 15:24:36 -0800

    Update getUsers.php

--- getUsers.php
+++ getUsers.php
@@ -8,7 +8,7 @@ try {
 
 $group_id =$_POST["group_id"];
 
-$query = "SELECT id, username, users.latitude, users.longitude, (SELECT SUM(score) FROM tasks WHERE user_id=users.id) AS score FROM users WHERE group_id='$group_id' ORDER BY DESC";
+$query = "SELECT id, username, users.latitude, users.longitude, (SELECT SUM(score) FROM tasks WHERE user_id=users.id) AS score FROM users WHERE group_id='$group_id', ORDER BY DESC";
 
 //"SELECT * FROM users WHERE username='$username' AND password = '$password';
 
